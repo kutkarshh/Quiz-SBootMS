5f37c9c446d9032d00e3e29930535c6ef634e8b5
Utkarsh Kumar <kumar6.utkarsh@gmail.com>
2024-09-07 14:41:29 +0530

    Added Load Balancer in the Quiz Service and Initialized ApiGateway

--- QuizService/src/main/java/com/quiz/services/QuestionClient.java
+++ QuizService/src/main/java/com/quiz/services/QuestionClient.java
@@ -8,7 +8,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import com.quiz.entities.Question;
 
-@FeignClient(url = "http://localhost:8082", value = "Question-Client")
+// @FeignClient(url = "http://localhost:8082", value = "Question-Client")
+
+// Now Using Load Balancer with Eureka Server and Microservices Architecture
+@FeignClient(name = "Question-Service")
 public interface QuestionClient {
 
     @GetMapping("/question/quiz/{quizId}")
